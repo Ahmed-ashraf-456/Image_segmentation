32c1e3765f737efc01a33465c670fd8e95570742
Hanya-Ahmad <hanya.taweel01@eng.st.cu.edu.eg>
2023-05-03 19:47:48 +0300

    minimized code repitition

--- segmentationLUV.py
+++ segmentationLUV.py
@@ -1,69 +1,70 @@
-from segmentation import KMeans
-from segmentation import AgglomerativeClustering
-from segmentation import MeanShift
-import cv2
-import numpy as np
-from luv import RGB2LUV
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-def apply_k_means_luv(source, k=5, max_iter=100):
+# from segmentation import KMeans
+# from segmentation import AgglomerativeClustering
+# from segmentation import MeanShift
+# import cv2
+# import numpy as np
+# from luv import RGB2LUV
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+
+
+
+# def apply_k_means_luv(image, k=5, max_iter=100):
+#     img=np.copy(image)
+#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     #convert to luv 
+#     img = RGB2LUV(img)
+
+#     # reshape image to points
+#     pixel_values = img.reshape((-1, 3))
+#     pixel_values = np.float32(pixel_values)
+
+#     # run clusters_num-means algorithm
+#     model = KMeans(k, max_iters=max_iter)
+#     y_pred = model.predict(pixel_values)
+
+#     centers = np.uint8(model.cent())
+#     y_pred = y_pred.astype(int)
+
+#     # flatten labels and get segmented image
+#     labels = y_pred.flatten()
+#     segmented_image = centers[labels.flatten()]
+#     segmented_image = segmented_image.reshape(img.shape)
+#     saved=mpimg.imsave("kmeans_luv.png", segmented_image)
+
+#     return segmented_image, labels
+
+# def apply_agglomerative_luv(image: np.ndarray, clusters_numbers: int = 2, initial_clusters: int = 25):
+
+#     # convert to RGB
+#     src = np.copy(image)
+#     src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+#     #convert to luv 
+#     src = RGB2LUV(src)
+#     agglomerative = AgglomerativeClustering(image=src, clusters_numbers=clusters_numbers,
+#                                             initial_k=initial_clusters)
+#     saved=mpimg.imsave("agg_luv.png", agglomerative.output_image)
+
+#     return agglomerative.output_image
+
+
+
+# def apply_mean_shift_luv(image: np.ndarray, threshold: int = 60):
     
-    # convert to RGB
-    img=np.copy(source)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    #convert to luv 
-    img = RGB2LUV(img)
 
-    # reshape image to points
-    pixel_values = img.reshape((-1, 3))
-    pixel_values = np.float32(pixel_values)
+#     src = np.copy(image)
 
-    # run clusters_num-means algorithm
-    model = KMeans(k, max_iters=max_iter)
-    y_pred = model.predict(pixel_values)
+#     # convert to RGB
+#     src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
+#     #convert to luv 
+#     src = RGB2LUV(src)
 
-    centers = np.uint8(model.cent())
-    y_pred = y_pred.astype(int)
+#     ms = MeanShift(image=src, threshold=threshold)
+#     ms.run_mean_shift()
+#     output = ms.get_output()
+#     saved=mpimg.imsave("meanshift_luv.png", output)
 
-    # flatten labels and get segmented image
-    labels = y_pred.flatten()
-    segmented_image = centers[labels.flatten()]
-    segmented_image = segmented_image.reshape(img.shape)
-    saved=mpimg.imsave("kmeans_luv.png", segmented_image)
-
-    return segmented_image, labels
-
-def apply_agglomerative_luv(source: np.ndarray, clusters_numbers: int = 2, initial_clusters: int = 25):
-
-    # convert to RGB
-    src = np.copy(source)
-    src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    #convert to luv 
-    src = RGB2LUV(src)
-    agglomerative = AgglomerativeClustering(source=src, clusters_numbers=clusters_numbers,
-                                            initial_k=initial_clusters)
-    saved=mpimg.imsave("agg_luv.png", agglomerative.output_image)
-
-    return agglomerative.output_image
-
-
-
-def apply_mean_shift_luv(source: np.ndarray, threshold: int = 60):
-    
-
-    src = np.copy(source)
-
-    # convert to RGB
-    src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    #convert to luv 
-    src = RGB2LUV(src)
-
-    ms = MeanShift(source=src, threshold=threshold)
-    ms.run_mean_shift()
-    output = ms.get_output()
-    saved=mpimg.imsave("meanshift_luv.png", output)
-
-    return output
+#     return output
 
 
 
@@ -71,7 +72,7 @@ def apply_mean_shift_luv(source: np.ndarray, threshold: int = 60):
 
 #     img = cv2.imread('seg-image.png')
 #     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     segmentedimg,labels= apply_k_means_luv(source=img)
+#     segmentedimg,labels= apply_k_means_luv(image=img)
 
 #     plt.figure()
 
--- segmentationRGB.py
+++ segmentationRGB.py
@@ -1,74 +1,60 @@
 from segmentation import KMeans
 from segmentation import AgglomerativeClustering
 from segmentation import MeanShift
-from segmentation import regionGrow
-from segmentation import Point
+# from segmentation import regionGrow
+# from segmentation import Point
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-def apply_k_means_rgb(source, k=5, max_iter=100):
-    
-    # convert to RGB
-    img=np.copy(source)
+from luv import RGB2LUV
+
+def kmeans(image, k=5, max_iter=100, luv=False):
+    img=np.copy(image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-
-    # reshape image to points
-    pixel_values = img.reshape((-1, 3))
-    pixel_values = np.float32(pixel_values)
-
-    # run clusters_num-means algorithm
+    if(luv):
+        img = RGB2LUV(img)
+    pixel_vals = img.reshape((-1, 3))
+    pixel_vals = np.float32(pixel_vals)
     model = KMeans(k, max_iters=max_iter)
-    y_pred = model.predict(pixel_values)
-
+    y_pred = model.predict(pixel_vals)
     centers = np.uint8(model.cent())
     y_pred = y_pred.astype(int)
-
     # flatten labels and get segmented image
     labels = y_pred.flatten()
     segmented_image = centers[labels.flatten()]
     segmented_image = segmented_image.reshape(img.shape)
-    saved=mpimg.imsave("kmeans_rgb.png", segmented_image)
-
+    saved=mpimg.imsave("output.png", segmented_image)
     return segmented_image, labels
 
-def apply_agglomerative_rgb(source: np.ndarray, clusters_numbers: int = 2, initial_clusters: int = 25):
-
-    # convert to RGB
-    src = np.copy(source)
+def agglomerative(image, clusters_numbers=2, initial_clusters= 25, luv=False):
+    src = np.copy(image)
     src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    
-    
-    agglomerative = AgglomerativeClustering(source=src, clusters_numbers=clusters_numbers,
+    if(luv):
+        print("LUVVVV")
+        src = RGB2LUV(src)
+    agglomerative = AgglomerativeClustering(image=src, clusters_numbers=clusters_numbers,
                                             initial_k=initial_clusters)
-    saved=mpimg.imsave("agg_rgb.png", agglomerative.output_image)
-
+    saved=mpimg.imsave("output.png", agglomerative.output_image)
     return agglomerative.output_image
 
-
-
-def apply_mean_shift_rgb(source: np.ndarray, threshold: int = 60):
-    
-
-    src = np.copy(source)
-
+def mean_shift(image, threshold= 60, luv=False):
+    src = np.copy(image)
     # convert to RGB
     src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-    
-    
+    if(luv):
+        src = RGB2LUV(src)
 
-    ms = MeanShift(source=src, threshold=threshold)
+    ms = MeanShift(image=src, threshold=threshold)
     ms.run_mean_shift()
     output = ms.get_output()
-    saved=mpimg.imsave("meanshift_rgb.png", output)
+    saved=mpimg.imsave("output.png", output)
 
     return output
 
-# def apply_region_growing(source: np.ndarray,threshold):
-    
+# def apply_region_growing(image: np.ndarray,threshold):
 
-#     src = np.copy(source)
+#     src = np.copy(image)
     
 #     src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
 #     src = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
@@ -88,21 +74,12 @@ def apply_mean_shift_rgb(source: np.ndarray, threshold: int = 60):
 
 if __name__ == "__main__":
 
-    img = cv2.imread('seg-image.png')
+    img = cv2.imread('snf.jpg')
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    segmentedimg= apply_agglomerative_rgb(source=img, clusters_numbers= 5)
-    # segmentedimg= apply_region_growing(source=img, threshold= 10)
+    segmentedimg= agglomerative(image=img, clusters_numbers=5,luv=False)
+    cv2.imshow('image' , img)
+    output = cv2.imread('output.png')
 
-    plt.figure()
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(img_rgb)
-    plt.axis('off')
-    plt.title('Original image')
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(segmentedimg)
-    plt.axis('off')
-    plt.title(f'Segmented image')
-
-    plt.show()
+    cv2.imshow('output' ,  output )
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
